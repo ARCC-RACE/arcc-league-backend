fac1bc5eccb379e9663d4f689ec93c57cf34161b
Caelin Sutch <caelinsutch@Caelins-MacBook-Pro.local>
2019-08-25 14:59:49 -0700

    UPdated email reset with working email reset

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -1,28 +1,46 @@
-/*
- * Copyright (c) Akveo 2019. All Rights Reserved.
- * Licensed under the Single Application / Multi Application License. 
- * See LICENSE_SINGLE_APP / LICENSE_MULTI_APP in the 'docs' folder for license information on type of purchased license.
- */
+const nodemailer = require('nodemailer');
 
 const config = require('config');
 const logger = require('../utils/logger');
 
 const { domain } = config.get('frontEnd');
 
-function doSend(email, text) {
-  logger.info(text);
+// TODO There's a way to do this with SMTP which will send from arcc.ai but I'm too lazy to setup right now :)
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'caelinsutch@gmail.com',
+    pass: 'Coolstar20!',
+  },
+});
+
+function doSend(email, subject, html) {
+  const mailOptions = {
+    from: 'caelinsutcb@arcc.ai', // sender address
+    to: email, // list of receivers
+    subject, // Subject line
+    html, // HTML body
+  };
+  transporter.sendMail(mailOptions,  (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
   return Promise.resolve(true);
 }
 
+// TODO Get cool email reset template going here @Alden I'm looking at you
 function sendResetPasswordEmail(email, fullName, token) {
-  const text = `Hello ${fullName},`
+  const html = `Hello ${fullName},`
   + '\nWe have received password reset request. '
   + `To do this, please proceed at ${domain}/#/auth/reset-password?reset_password_token=${token}`
   + '\nIf it wasn\'t you, take no action or contact support.'
   + '\n\nThank you,'
-  + '\nSupport team.';
+  + '\nSupport team at ARCC.';
 
-  return doSend(email, text);
+  return doSend(email, 'Reset Password', html);
 }
 
 module.exports = {
