eae032745339702af16b405903ba7b384db453dd
Caelin Sutch <caelinsutch@gmail.com>
2019-11-22 18:15:07 -0800

    Time for CORS

--- app.js
+++ app.js
@@ -33,9 +33,11 @@ function clientErrorHandler(err, req, res, next) {
   }
 }
 
+app.use(cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 app.use(bodyParser.urlencoded({ extended: false }));
