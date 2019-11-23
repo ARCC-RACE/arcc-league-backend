23f50ff1d8a7c9c75e06e6c176365294b1dec65b
Caelin Sutch <caelinsutch@gmail.com>
2019-11-22 17:49:01 -0800

    Trying to fix dumb cross origin error

--- app.js
+++ app.js
@@ -33,7 +33,11 @@ function clientErrorHandler(err, req, res, next) {
   }
 }
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
